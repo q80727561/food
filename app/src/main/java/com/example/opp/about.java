64729c5062b6b8fd4993e54678d28b536aedc131
YOUR NAME <YOUR EMAIL ADDRESS>
2017-06-22 15:39:27 +0800

    test

--- app/src/main/java/com/example/opp/about.java
+++ app/src/main/java/com/example/opp/about.java
@@ -24,14 +24,10 @@ public class about extends DialogFragment implements View.OnClickListener {
         if (getTag() == "關於作者"){
             mToolbar.setTitle("關於作者");
             TextView.setText("我叫陳居賢\n非常願意學習新的知識，希望能有機會到貴公司上班，希望可以有面試的機會。");
-
         }
         else{
             mToolbar.setTitle("關於作品");
             TextView.setText("作品的遠端資料庫是我免費伺服器架的。\n資料庫是讀取事先從mysql匯出的csv，資料很少以後會增加。\n如果發現任何問題請回報給我。");
-
-
-
         }
         return view;
     }
